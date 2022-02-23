e7ea4347904d80075ed4e7ffe3ae68322fc83295
iqbalrief <iqbal98arief@gmail.com>
2022-02-23 19:37:04 +0700

    Initial Commit

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable testing-library/no-container */
+import React from 'react'
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router} from 'react-router-dom'
+
+import Button from './index'
+
+test("Should not allowed click button of isDisabled is present", () => {
+    const {container} = render(<Button isDisabled></Button>)
+    
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(container.querySelector('span.disabled')).toBeInTheDocument()
+})
+
+test("Should render loading/spinner", () => {
+    const {container, getByText} = render(<Button isLoading></Button>)
+
+    expect(getByText(/loading/i)).toBeInTheDocument();
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(container.querySelector("span")).toBeInTheDocument()
+
+})
+
+test("Should render <a> tag", () => {
+    const {container} = render(<Button type="link" isExternal></Button>)
+    
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(container.querySelector("a")).toBeInTheDocument();
+})
+
+test("Should render <Link> component", () => {
+    const {container} = render(
+    <Router>
+        <Button href="" type="link">
+        </Button>
+    </Router>)
+    
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(container.querySelector("a")).toBeInTheDocument();
+})
